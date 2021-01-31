ea9b442c178d3b31d675192c0b0fcfc7a0fdecd1
João Victor de Mesquita <mesquitajoaoc@outlook.com>
2021-01-31 00:27:02 -0300

    code looks fine by now

---
+++ Training/qlearning_evaluate.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pickle
+from qlearning_training import  reset_enviroment, initialize_state_matrix, identifiesgoal_state, identifies_state
+
+action_space = np.array([0, 1, 2, 3])
+
+def identifies_index(state):
+    for i in range(enviromentsize):
+        for j in range(enviromentsize):
+                if state_matrix[i][j] == state:
+                    return i, j
+
+def identifies_state_matrix(i, j):
+    return state_matrix[i][j]
+# verificar se tomando uma nova decisão não tomei a mesma anteriormente pra evitar caminhos longos
+
+def next_step(action, state, goal_state):
+  
+    if state == goal_state:
+        return 10, state
+    else:
+        i, j = identifies_index(state)
+
+        #up
+        if action == 0 and i > 0:
+                i -= 1
+            # Move left
+        elif action == 1 and j > 0:
+                j -= 1
+            # Move down
+        elif action == 2 and i < enviromentsize - 1:
+                i += 1
+            # Move right
+        elif action == 3 and j < enviromentsize - 1:
+                j += 1
+
+        next_state = identifies_state_matrix(i,j)
+        reward = -1
+        return reward, int(next_state) 
+
+def select_optimal_action(state):
+
+    optimal = np.argmax(Q[state], axis=0)
+
+    if Q[state][optimal] == 0.:
+        optimal = np.argmin(Q[state], axis=0)
+        print(state, optimal, Q[state][optimal])
+    return optimal
+
+def select_optimal_path(q_table, enviroment):
+    enviroment[0][0] = 1
+    # reset enviroment to learn a new goal
+    i, j = identifies_state(enviroment, enviromentsize)
+    k, l = identifiesgoal_state(enviroment, enviromentsize)
+    state = int(state_matrix[i][j])
+    goal_state = int(state_matrix[k][l])
+    states = []
+    states.append(state)
+    print(state, goal_state, '\n')
+    print('\n', q_table, '\n', enviroment)
+    old_state, reward, next_state = 0, 0, 0
+    done = False
+    while(not done):
+        action = select_optimal_action(state)
+        reward, next_state = next_step(action,state,goal_state)
+        state = next_state
+        states.append(state)
+        print(states)
+        print(state, goal_state, '\n', enviroment, '\n', q_table)
+        if reward == 10:
+            done = True
+    print(states, '\n', q_table)
+
+
+def main():
+    global state_matrix, enviromentsize, Q
+    enviromentsize = 4
+    env = np.zeros((enviromentsize, enviromentsize))
+    env = reset_enviroment(env, enviromentsize)
+    state_matrix = initialize_state_matrix(
+        np.zeros((enviromentsize, enviromentsize)), enviromentsize)
+
+    with open('pickle/Q_0_1.pickle', "rb") as read:
+        Q = pickle.load(read)
+
+    select_optimal_path(Q, env)
+
+
+if __name__ == '__main__':
+    main()
--- Training/qlearning_training.py
+++ Training/qlearning_training.py
@@ -1,4 +1,3 @@
-# pylint: disable=locally-disabled, multiple-statements, fixme, line-too-long, unused-variable
 import itertools
 import matplotlib
 import numpy as np
@@ -11,205 +10,161 @@ from matplotlib import pyplot as plt
 
 
 # parameters of learning
-alpha = 0.5  # learning rate, learn more quickly if alpha is closer to 1
+alpha = 0.5 # learning rate, learn more quickly if alpha is closer to 1
 gamma = 0.6  # use a higher gamma for smaller spaces because we value later rewards rather than former rewards
-epsilon = 0  # agent can do 100% exploitation (0) or 100% exploration (1)
+epsilon = 0.05  # agent can do 100% exploitation (0) or 100% exploration (1)
 
-NUM_EPISODES = 10000
+action_space = [0,1,2,3]
+
+NUM_EPISODES = 700
 
 '''action space:
     0 - south (up)
     1 - north(down)
     2 - east (right)
     3 - west (left)'''
-action_space = np.array([0, 1, 2, 3])
 
 
-def select_optimal_action(q_table, state):
-    return np.argmax(q_table[state], axis=0)
+def select_optimal_action(state, actions_allowed):
+    #print('cheguei no seleciona melhor', state)
+    for i in range(0, 3):
+        if i in actions_allowed:
+            pass
+        else:
+            Q[state][i] = 0
+    #print(Q[state])
+    optimal = np.argmax(Q[state], axis=0)
+    if Q[state][optimal] == 0.:
+        optimal = np.argmin(Q[state], axis=0)
+    #print('select optimals', optimal)
+    return optimal
 
 
-def check_old_state(old_state, new_state):
-    if new_state == old_state:
-        return -10, old_state
-    else:
-        return 0, new_state
+def identifies_index(state):
+    for i in range(enviromentsize):
+        for j in range(enviromentsize):
+                if state_matrix[i][j] == state:
+                    return i, j
 
-
+def identifies_state_matrix(i, j):
+    return state_matrix[i][j]
 # verificar se tomando uma nova decisão não tomei a mesma anteriormente pra evitar caminhos longos
-def next_step(q_table, enviroment, action, state, old_state, goal_state):
-    #print('estou no estado', state, 'acao', action, 'vim do estado', old_state )
+def next_step(action, state, goal_state):
+  
     if state == goal_state:
-        return 20, state
+        return 10, state
+    elif state == trap_state:
+        return -5, state
     else:
-        if state == 0:
-            if action == 0 or action == 3:
-                return -10, state
-            elif action == 1:
-                return 0, state+4
-            elif action == 2:
-                return 0, state+1
+        i, j = identifies_index(state)
 
-        elif state == 1 or state == 2:
-            if action == 0:
-                return -10, state
-            elif action == 1:
-                reward, new_state = check_old_state(old_state, state+4)
-                return reward, new_state
-            elif action == 2:
-                reward, new_state = check_old_state(old_state, state+1)
-                return reward, new_state
-            elif action == 3:
-                reward, new_state = check_old_state(old_state, state-1)
-                return reward, new_state
+        #up
+        if action == 0 and i > 0:
+                i -= 1
+            # Move left
+        elif action == 1 and j > 0:
+                j -= 1
+            # Move down
+        elif action == 2 and i < enviromentsize - 1:
+                i += 1
+            # Move right
+        elif action == 3 and j < enviromentsize - 1:
+                j += 1
 
-        elif state == 5 or state == 6 or state == 9 or state == 10:
-            if action == 0:
-                reward, new_state = check_old_state(old_state, state-4)
-                return reward, new_state
-            elif action == 1:
-                reward, new_state = check_old_state(old_state, state+4)
-                return reward, new_state
-            elif action == 2:
-                reward, new_state = check_old_state(old_state, state+1)
-                return reward, new_state
-            elif action == 3:
-                reward, new_state = check_old_state(old_state, state-1)
-                return reward, new_state
+        next_state = identifies_state_matrix(i,j)
+        reward = -1
+        return reward, int(next_state)
 
-        elif state == 3:
-            if action == 0 or action == 2:
-                return -10, state
-            elif action == 1:
-                reward, new_state = check_old_state(old_state, state+4)
-                return reward, new_state
-            elif action == 3:
-                reward, new_state = check_old_state(old_state, state-1)
-                return reward, new_state
-
-        elif state == 7 or state == 11:
-            if action == 0:
-                reward, new_state = check_old_state(old_state, state-4)
-                return reward, new_state
-            elif action == 1:
-                reward, new_state = check_old_state(old_state, state+4)
-                return reward, new_state
-            elif action == 2:
-                return -10, state
-            elif action == 3:
-                reward, new_state = check_old_state(old_state, state-1)
-                return reward, new_state
-
-        elif state == 4 or state == 8:
-            if action == 0:
-                reward, new_state = check_old_state(old_state, state-4)
-                return reward, new_state
-            elif action == 3:
-                return -10, state
-            elif action == 1:
-                reward, new_state = check_old_state(old_state, state+4)
-                return reward, new_state
-            elif action == 2:
-                reward, new_state = check_old_state(old_state, state+1)
-                return reward, new_state
-
-        elif state == 12:
-            if action == 0:
-                reward, new_state = check_old_state(old_state, state-4)
-                return reward, new_state
-            elif action == 3 or action == 1:
-                return -10, state
-            elif action == 2:
-                reward, new_state = check_old_state(old_state, state+1)
-                return reward, new_state
-
-        elif state == 13 or state == 14:
-            if action == 0:
-                reward, new_state = check_old_state(old_state, state-4)
-                return reward, new_state
-            elif action == 3:
-                reward, new_state = check_old_state(old_state, state-1)
-                return reward, new_state
-            elif action == 1:
-                return -10, state
-            elif action == 2:
-                reward, new_state = check_old_state(old_state, state+1)
-                return reward, new_state
-
-        elif state == 15:
-            if action == 0:
-                reward, new_state = check_old_state(old_state, state-4)
-                return reward, new_state
-            elif action == 3:
-                reward, new_state = check_old_state(old_state, state-1)
-                return reward, new_state
-            elif action == 1 or action == 2:
-                return -10, state
+def checking_allowed_spaces(state):
+    if (state == 5 or state == 6 or state == 9 or state == 10):
+        return np.array([0, 1, 2, 3])
+    elif state == 0:
+        return np.array([1, 2])
+    elif (state == 1 or state == 2):
+        return np.array([1, 2, 3])
+    elif state == 3:
+        return np.array([1,3])
+    elif state == 4 or state == 8:
+        return np.array([0, 1, 2])
+    elif state == 7 or state == 11:
+        return np.array([0, 1, 3])
+    elif state == 12:
+        return np.array([0, 2])
+    elif state == 13 or state == 14:
+        return np.array([0, 2, 3])
+    elif state == 15:
+        return np.array([0, 3])
 
 
-def update(q_table, enviroment, state, old_state, episode):
+def update( enviroment, state, old_state, episode, steps):
+    # print('updating')
+    done = False
+    #action_space = checking_allowed_spaces(state)
     if random.uniform(0, 1) < epsilon:
         action = random.choice(action_space)
+
     else:
-        action = select_optimal_action(q_table, state)
+
+        action = select_optimal_action(state, action_space)
 
     # get the reward of choosing current action and the next state
-    reward, next_state = next_step(
-        q_table, enviroment, action, state, old_state, goal_state)
+    reward, next_state = next_step(action, state, goal_state)
 
+    if reward == 10 or reward==-5:
+        done = True
     # stores the old_state to not going back avoiding loopings in the same square
+    
     old_state = state
-    old_q_value = q_table[state][action]
 
     # get maximum q_value for action in next state
-    next_max_state_q_value = np.argmax(q_table[next_state], axis=0)
+    
+    next_max_state_q_value = np.max(Q[next_state])
 
-    if episode == NUM_EPISODES and reward == 20:
-        new_q_value = (1-alpha) * old_q_value + alpha * \
-            (-1 + gamma * next_max_state_q_value)
-    else:
-        new_q_value = (1-alpha) * old_q_value + alpha * \
-            (reward + gamma * next_max_state_q_value)
-    q_table[state][action] = new_q_value
-    return next_state, reward, old_state
+    Q[state][action] += alpha*(reward + gamma*next_max_state_q_value - Q[state][action])
+
+    return next_state, reward, old_state, done
 
 
-def training_agent(q_table, enviroment, num_episodes, env_size):
-    global goal_state, steps, rewards, training_start, training_end, penalties
+def training_agent(enviroment, num_episodes, env_size):
+    global goal_state, steps, rewards, training_start, training_end, penalties, trap_state
     training_start = time()
     steps = []
     rewards = []
     penalties = []
-    for _ in range(num_episodes):
+    states = []
+    for episode in range(num_episodes):
+        states = []
         enviroment = reset_enviroment(enviroment, env_size)
         # reset enviroment to learn a new goal
         i, j = identifies_state(enviroment, env_size)
         k, l = identifiesgoal_state(enviroment, env_size)
+        m, n = identifies_trap(enviroment, env_size)
         state = int(state_matrix[i][j])
         goal_state = int(state_matrix[k][l])
+        trap_state =  int(state_matrix[m][n])
+        states.append(state)
         epochs = 0
         num_penalties, reward, total_reward, old_state = 0, 0, 0, 0
-        while int(reward) < 20:
-            state, reward, old_state = update(
-                q_table, enviroment, state, old_state, num_episodes)
+        done = False
+        while not done:
+            state, reward, old_state, done = update(enviroment, state, old_state, num_episodes, epochs)
             total_reward += reward
-            if state != old_state:
-                epochs += 1
-            if reward == -10:
-                num_penalties += 1
-
+            epochs += 1
+            #print(state, reward, old_state, done, goal_state)
+            states.append(state)
         # store steps and rewards for each episode
         steps.append(epochs)
         rewards.append(total_reward)
         penalties.append(num_penalties)
-
-        print("Time steps: {}, Penalties: {}, Reward: {}, Goal State: {}".format(
-            epochs, num_penalties, total_reward, goal_state))
+        
+        print("Time steps: {}, Penalties: {}, Reward: {}, Goal State: {}, Epsilon:{}, Episode:{}".format(
+            epochs, num_penalties, total_reward, goal_state, epsilon, episode))
+        print(states)
 
     training_end = time()
 
-    print(rewards)
-    return q_table, enviroment
+    # print(rewards)
+
 
 
 def evaluate_training():
@@ -265,8 +220,10 @@ def reset_enviroment(enviroment, env_size):  # changes the goal to another place
     i, j = indices[0], indices[1]
     if(i == 0 == j):
         j += 1
-    enviroment[i][j] = 20
+    enviroment[2][3] = 20
+    enviroment[1][1] = -1
     enviroment[0][0] = 1
+
     return enviroment
 
 
@@ -292,8 +249,12 @@ def identifiesgoal_state(enviroment, env_size):
             if enviroment[i][j] == 20:
                 return i, j
 
+def identifies_trap(enviroment, env_size):
+    for i in range(env_size):
+        for j in range(env_size):
+            if enviroment[i][j] == -1:
+                return i, j
 # plot desired matrix
-
 
 def plot_matrix(matrix, x_size, y_size):
     figure, ax = plt.subplots()
@@ -303,23 +264,23 @@ def plot_matrix(matrix, x_size, y_size):
             text = ax.text(j, i, matrix[i, j],
                            ha="center", va="center", color="w")
     figure.tight_layout()
-    plt.savefig("imgs/Q_table.png")
+    plt.savefig("imgs/Q.png")
 
 
 def main():
-    global state_matrix
+    global state_matrix, enviromentsize, Q
     enviromentsize = 4
     state_size = enviromentsize*enviromentsize  # defines environment
     env = np.zeros((enviromentsize, enviromentsize))
     env = reset_enviroment(env, enviromentsize)
     actions_size = enviromentsize  # lef, right, up and down
-    state_matrix = initialize_state_matrix(
-        np.zeros((enviromentsize, enviromentsize)), enviromentsize)
+    state_matrix = initialize_state_matrix(np.zeros((enviromentsize, enviromentsize)), enviromentsize)
     Q = np.zeros((state_size, actions_size))  # initializing q-table with zeros
-    Q, enviroment = training_agent(Q, env, NUM_EPISODES, enviromentsize)
+    Q.shape
+    training_agent(env, NUM_EPISODES, enviromentsize)
     evaluate_training()
     print(Q)
-    with open('pickle/q_table.pickle', "wb") as write:
+    with open('pickle/Q_0_1.pickle', "wb") as write:
         pickle.dump(Q, write)
 
 
